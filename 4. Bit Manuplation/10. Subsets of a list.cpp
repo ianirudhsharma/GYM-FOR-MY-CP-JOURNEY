7613410c4ee7dd7a45d7c3de46dccc770a521caa
Shubhra Bhattacharjee <shubhra.bhattacharjeee@gmail.com>
2024-05-01 10:39:05 +0530

    Update 10. Subsets of a list.cpp

--- 4. Bit Manuplation/10. Subsets of a list.cpp
+++ 4. Bit Manuplation/10. Subsets of a list.cpp
@@ -18,37 +18,23 @@ vector<string> split(const string &s, char delim)
  
 // Function to find all subsets of given set. Any repeated
 // subset is considered only once in the output
-int printPowerSet(int arr[], int n)
-{
-    vector<string> list;
- 
-    /* Run counter i from 000..0 to 111..1*/
-    for (int i = 0; i < (int) pow(2, n); i++)
-    {
-        string subset = "";
- 
-        // consider each element in the set
-        for (int j = 0; j < n; j++)
-        {
-            // Check if jth bit in the i is set. If the bit
-            // is set, we consider jth element from set
-            if ((i & (1 << j)) != 0)
-                subset += to_string(arr[j]) + "|";
+void printPowerSet(int arr[], int n) {
+    set<vector<int>> subsets;
+
+    for (int i = 0; i < (1 << n); ++i) {
+        vector<int> subset;
+        for (int j = 0; j < n; ++j) {
+            if (i & (1 << j)) {
+                subset.push_back(arr[j]);
+            }
         }
- 
-        // if subset is encountered for the first time
-        // If we use set<string>, we can directly insert
-        if (find(list.begin(), list.end(), subset) == list.end())
-            list.push_back(subset);
+        subsets.insert(subset);
     }
- 
-    // consider every subset
-    for (string subset : list)
-    {
-        // split the subset and print its elements
-        vector<string> arr = split(subset, '|');
-        for (string str: arr)
-            cout << str << " ";
+
+    for (const auto& subset : subsets) {
+        for (const auto& element : subset) {
+            cout << element << " ";
+        }
         cout << endl;
     }
 }
